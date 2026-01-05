d7f4d48e0f7c778c927473798523e2c7afe85ddd
giapnguyen <giapnguyen2004@gmail.com>
2026-01-05 17:26:56 +0700

    fixed name topic mqtt

--- src/config/mqttTopics.js
+++ src/config/mqttTopics.js
@@ -1,17 +1,22 @@
 export const MQTT_TOPICS = {
-    SLOT_UPDATE: 'iot4/parking/sensors/update',   
+    SLOT_UPDATE: 'parking/update',   
 
     // LED Control
-    LED_CONTROL: 'iot/control/led',
+    //LED_CONTROL: 'iot/control/led',
 
     // Fire Warning - DHT11 Temperature Sensor (ESP32 -> Server)
-    FIRE_TEMPERATURE: 'iot4/parking/sensors/fire/temperature',
+    FIRE_TEMPERATURE: 'parking/alert',
 
     // Fire Warning Control (Server -> ESP32)
-    FIRE_BUZZER_CONTROL: 'iot4/parking/device/fire/buzzer/control',
+    //FIRE_BUZZER_CONTROL: 'iot4/parking/device/fire/buzzer/control',
 
     // Gate
-    GATE_CONTROL: 'iot4/parking/device/gate/control',
+    GATE_CONTROL: 'parking/gate_cmd',
 }
 
 export default MQTT_TOPICS
+
+// // Topic MQTT
+// const char* topic_update = "parking/update";
+// const char* topic_alert  = "parking/alert";
+// const char* topic_led    = "parking/gate_cmd";
